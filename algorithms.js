22c60383d6c94a513187dbc311d83b0d40629990
DQCoding1 <diegoabquintero@gmail.com>
2023-03-24 12:11:45 -0500

    RETURN THE SUM OF THE MIN AND MAX OF AN ARRAY

--- algorithms.js
+++ algorithms.js
@@ -3,3 +3,34 @@
 //   num % 2 === 0 ? console.log( `${num} : even`) : console.log(`${num} : odd`);
 // }
 // evenOrOdd(239)
+
+
+
+
+
+
+
+
+
+
+
+
+// RETURN THE SUM OF THE MIN AND MAX OF AN ARRAY
+// function sumOfMinAndMax(arr){
+//   let min = arr[0]
+//   let max = arr[0]
+//   arr.forEach(num => {
+//     if (num < min){
+//       min = num
+//     } 
+//     if (num > max){
+//       max = num
+//     }
+//   })
+//   return min + max
+  // ANOTHER WAY TO SOLVE IT
+  // let max = (Math.min(...arr));
+  // let min = (Math.max(...arr));
+  // return min + max
+// }
+// console.log(sumOfMinAndMax([3,5,7,2,41,8]))
